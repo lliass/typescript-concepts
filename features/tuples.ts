9aa838657e6197d4f862302c50f3df227ea551a3
Elias Lopes <elias.lopes@mevosaude.com.br>
2022-11-28 20:13:07 -0300

    feature: Add tuples examples

---
+++ features/tuples.ts
@@ -0,0 +1,19 @@
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40
+};
+
+//Type Alias
+type Drink = [string, boolean, number];
+
+const pepsi: Drink = ['brown', true, 40];
+const sprite: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0];
+
+const carSpecs: [number, number] = [400, 3354];
+
+const carSpecsObject: {horsePower: number, weight: number} = {
+  horsePower: 400,
+  weight: 33
+};
